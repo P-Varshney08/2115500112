9bf0de0f4a1b24edbf976363472c26115e70d2a9
priya varshney <priyavarshney081003@gmail.com>
2024-07-12 12:07:33 +0530

    up

--- 1/backend/routes/user.route.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const {SignUp,login}=require("../controllers/user.controller")
-router.post('/signup',SignUp);
-router.post('/login',login);
-module.exports=router;
